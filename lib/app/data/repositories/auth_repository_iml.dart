add5133de822d0d896d4212719f69213754a5ee5
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-02 17:20:42 +0500

    imp

--- lib/app/data/repositories/auth_repository_iml.dart
+++ lib/app/data/repositories/auth_repository_iml.dart
@@ -43,7 +43,6 @@ class AuthRepositoryImpl implements AuthRepository {
     }
   }
 
-
   @override
   Future<Either<NetworkFailure, AuthUser>> signUpWithEmail(
       SignUpRequestModel request) async {
