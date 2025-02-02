b347db9372ac58e112cc3f1354f52dcee17acf04
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-02 17:00:59 +0500

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,6 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         initialRoute: AppRoutes.todo,
         getPages: AppPages.pages,
-
         builder: (context, child) => MediaQuery(
           data: MediaQuery.of(context).copyWith(
             textScaler: TextScaler.noScaling,
