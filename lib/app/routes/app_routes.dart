6701104ddeb1a42877d1f7844fc3d0089d6cf40f
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-02 13:17:25 +0500

    imp

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -1,4 +1,35 @@
 // lib/app/routes/app_routes.dart
 class AppRoutes {
   static const todo = '/todo';
+
+  /// main
+  static const main = "/main";
+  static const home = "$main/home";
+  static const saved = "$main/saved";
+  static const leaderboard = "$main/leaderboard";
+  static const profile = "$main/profile";
+
+  /// auth
+  static const onboarding = "/onboarding";
+  static const signIn = "$onboarding/sign_in";
+  static const signUp = "$onboarding/sign_up";
+  static const forgotPassword = "$signIn/forgot_password";
+  static const confirmOTP = "$forgotPassword/confirm_otp";
+  static const resetPassword = "$forgotPassword/reset_password";
+
+  /// detail unit
+  static const detailUnit = "$home/detail_unit";
+  static const quiz = "$detailUnit/quiz";
+  static const result = "$quiz/result";
+
+  /// detail saved
+  static const detailSaved = "$saved/detail_saved";
+  static const newCategory = "$saved/new_saved";
+
+  /// leaderboard
+  static const detailProfile = "$leaderboard/detail_profile";
+
+  /// profile
+  static const settings = "$profile/settings";
+  static const editProfile = "$profile/edit_profile";
 }
