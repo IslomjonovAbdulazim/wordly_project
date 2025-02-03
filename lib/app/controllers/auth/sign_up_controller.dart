bed27305abba02e6ff1c6de50be0f0bf25243ce1
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-03 13:48:21 +0500

    imp

--- lib/app/controllers/auth/sign_up_controller.dart
+++ lib/app/controllers/auth/sign_up_controller.dart
@@ -1,6 +1,24 @@
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class SignUpController extends GetxController {
+  RxBool isLoading = false.obs;
+  RxBool eyeHidden = true.obs;
 
+  TextEditingController fullname = TextEditingController();
+  Rx<FocusNode> fullnameFocus = FocusNode().obs;
+  TextEditingController emailController = TextEditingController();
+  Rx<FocusNode> emailFocus = FocusNode().obs;
+  TextEditingController passwordController = TextEditingController();
+  Rx<FocusNode> passwordFocus = FocusNode().obs;
+
+  void unFocus() {
+    emailFocus.value.unfocus();
+    passwordFocus.value.unfocus();
+    fullnameFocus.value.unfocus();
+  }
+
+  void toggleEye() {
+    eyeHidden.value = !(eyeHidden.value);
+  }
 }
-
