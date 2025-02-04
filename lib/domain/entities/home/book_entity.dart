95949e4b8b6af120322fddc9abf1c8186d95b3c6
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-04 16:48:34 +0500

    imp

--- lib/domain/entities/home/book_entity.dart
+++ lib/domain/entities/home/book_entity.dart
@@ -11,3 +11,42 @@ class BookEntity {
     required this.id,
   });
 }
+
+final List<BookEntity> essentialBooks = [
+  BookEntity(
+    id: 1,
+    name: "Thinking, Fast and Slow",
+    number: 1,
+    image: "https://images-na.ssl-images-amazon.com/images/I/71H6Fz4opzL.jpg",
+  ),
+  BookEntity(
+    id: 2,
+    name: "Atomic Habits",
+    number: 2,
+    image: "https://images-na.ssl-images-amazon.com/images/I/91bYsX41DVL.jpg",
+  ),
+  BookEntity(
+    id: 3,
+    name: "The 7 Habits of Highly Effective People",
+    number: 3,
+    image: "https://images-na.ssl-images-amazon.com/images/I/71uAI28kJuL.jpg",
+  ),
+  BookEntity(
+    id: 4,
+    name: "The Power of Now",
+    number: 4,
+    image: "https://images-na.ssl-images-amazon.com/images/I/71mOaiB1M+L.jpg",
+  ),
+  BookEntity(
+    id: 5,
+    name: "Deep Work",
+    number: 5,
+    image: "https://images-na.ssl-images-amazon.com/images/I/71Aqjxxu4QL.jpg",
+  ),
+  BookEntity(
+    id: 6,
+    name: "The Subtle Art of Not Giving a F*ck",
+    number: 6,
+    image: "https://images-na.ssl-images-amazon.com/images/I/71QKQ9mwV7L.jpg",
+  ),
+];
