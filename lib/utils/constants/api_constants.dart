295887c601d00f60c3fc49f1aedc83559886b389
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-04 11:04:36 +0500

    imp

--- lib/utils/constants/api_constants.dart
+++ lib/utils/constants/api_constants.dart
@@ -1,5 +1,7 @@
 class ApiConstants {
   static const String baseURL = "5.42.220.153:8003";
+
+  /// AUTH
   static const String resetPassword = "/api/v1/auth/auth/reset-password/";
   static const String forgotPassword = "/api/v1/auth/forgot-password";
   static const String register = "/api/v1/auth/register/";
@@ -7,4 +9,9 @@ class ApiConstants {
   static const String verifyOTP = "/api/v1/auth/verify-otp";
   static const String token = "/api/v1/auth/api/token/";
   static const String refreshToken = "/api/v1/auth/api/token/refresh/";
+
+  /// HOME
+  static const String books = "/api/v1/books/";
+  static const String units = "/api/v1/books/{book_id}/units/";
+  static const String randomQuiz = "/api/v1/random-quiz/";
 }
