bf4115db37f332164b26d2bfa27c25ef839bc6aa
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-02-05 21:57:46 +0500

    imp

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,6 @@ void main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     final ThemeController themeController = Get.find<ThemeController>();
